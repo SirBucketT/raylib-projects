8b9bb665fbb4de34bd75650b888ad1ee222f0665
SirBucketT <tkpriva@icloud.com>
2025-01-16 13:09:49 +0100

    update

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include "raylib.h"
 
-int circleX = 50;
-int circleY = 50;
+int circleX = 900;
+int circleY = 450;
+int movementSpeed = 20;
 
 int main(void) {
 
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1800;
+    const int screenHeight = 900;
 
     InitWindow(screenWidth, screenHeight, "Raylib bullshit window");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_A)) {
+            circleX -= movementSpeed;
+        } else if (IsKeyDown(KEY_D)) {
+            circleX += movementSpeed;
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
