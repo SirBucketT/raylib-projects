7c2d854748a41effe01dd1c12c9db08c6cf15519
SirBucketT <tkpriva@icloud.com>
2025-01-16 15:54:02 +0100

    update

--- main.c
+++ main.c
@@ -9,8 +9,9 @@ int movementSpeed = 70;
 int width;
 int height;
 
-Color color;
 bool isAlive = true;
+bool bulletActive = false;
+bool gameOver = false;
 
 int main(void) {
 
@@ -19,6 +20,11 @@ int main(void) {
     screenSpace = screenHeight - (screenHeight / 25) - 30;
     playerX = screenWidth / 2;
     playerY = screenSpace;
+    float bulletX = playerX + 40;
+    float bulletY = playerY -40;
+    float bulletSpeedX = -40;
+    float bulletSpeedY = bulletSpeedX;
+    int bulletRadius = 5;
 
 
     InitWindow(screenWidth, screenHeight, "useless raylib");
@@ -32,10 +38,37 @@ int main(void) {
             playerX += movementSpeed;
         }
 
+        if (IsKeyPressed(KEY_SPACE) && !bulletActive) {
+            bulletX = playerX + screenWidth / 50;
+            bulletY = playerY;
+            bulletActive = true;
+        }
+        if (bulletActive) {
+            DrawCircle(bulletX, bulletY - 40, bulletRadius, WHITE);
+            bulletY += bulletSpeedY;
+            bulletX += bulletSpeedX;
+
+            if (bulletX - bulletRadius <= 0 || bulletX + bulletRadius >= screenWidth) {
+                bulletSpeedX *= -1;
+            }
+
+            if (bulletY - bulletRadius <= 0) {
+                bulletSpeedY *= -1;
+            }
+            if (bulletY >= screenHeight) {
+                bulletActive = false;
+                isAlive = false;
+            }
+        }
+
+        if (isAlive == true) {
+            DrawRectangle(playerX, playerY, screenWidth / 25, screenHeight/50, WHITE);
+        } else {
+            DrawText("GAME OVER!", screenWidth / 2 - 100, screenHeight / 2, 50, RED);
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawRectangle(playerX, playerY, screenWidth / 25, screenHeight/50, WHITE);
-        DrawCircle(playerX + 40, playerY - 40, 5, WHITE);
         EndDrawing();
     }
 
