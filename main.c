e232b59b24724e143d54977b2b650704c9efdc6b
SirBucketT <tkpriva@icloud.com>
2025-01-16 14:45:15 +0100

    update

--- main.c
+++ main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include "raylib.h"
 
-int circleX = 900;
-int circleY = 450;
-int movementSpeed = 20;
+int playerX;
+int playerY;
+int screenSpace;
+int movementSpeed = 70;
+
+int width;
+int height;
+
+Color color;
 bool isAlive = true;
 
 int main(void) {
 
     const int screenWidth = 1800;
     const int screenHeight = 900;
+    screenSpace = screenHeight - (screenHeight / 25) - 30;
+    playerX = screenWidth / 2;
+    playerY = screenSpace;
 
-    InitWindow(screenWidth, screenHeight, "Raylib bullshit window");
+
+    InitWindow(screenWidth, screenHeight, "useless raylib");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
 
-        if (IsKeyDown(KEY_A)) {
-            circleX -= movementSpeed;
-        } else if (IsKeyDown(KEY_D)) {
-            circleX += movementSpeed;
+        if (IsKeyDown(KEY_A) && playerX > 0) {
+            playerX -= movementSpeed;
+        } else if (IsKeyDown(KEY_D) && playerX + screenWidth / 25 < screenWidth) {
+            playerX += movementSpeed;
         }
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawCircle(circleX, circleY, 20, GRAY);
+        ClearBackground(BLACK);
+        DrawRectangle(playerX, playerY, screenWidth / 25, screenHeight/50, WHITE);
+        DrawCircle(playerX + 40, playerY - 40, 5, WHITE);
         EndDrawing();
     }
 
